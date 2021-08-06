cf045350c74dc9274d1200132cc758a02a237d2d
Abh29 <aboutifor@stud.pkfu.ru>
2021-08-06 04:08:37 -0400

    adding strings_verzahnt

---
+++ strings_verzahnen.c
@@ -0,0 +1,22 @@
+#include "strings_verzahnen.h"
+#include <stdio.h>
+
+void	strings_verzahnen(char *verzahnt, char *string0, char *string1)
+{
+	if (verzahnt == NULL)
+		return ;
+	while (*string0 && *string1)
+	{
+		*verzahnt++ = *string0++;
+		*verzahnt++ = *string1++;
+	}
+	if (*string0 || *string1)
+	{
+		if (*string1)
+			string0 = string1;
+		while (*string0)
+			*verzahnt++ = *string0++;
+	}
+	*verzahnt = 0;
+	return ;
+}
---
+++ strings_verzahnen.h
@@ -0,0 +1,7 @@
+#ifndef STRINGS_VERZAHNEN_H
+# define STRINGS_VERZAHNEN_H
+#include <stddef.h>
+
+void	strings_verzahnen(char *verzahnt, char *string0, char *string1);
+
+#endif
